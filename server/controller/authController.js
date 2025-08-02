275eda7e656241f76349d88e91b16e48e99d5819
Sthitapragnya <sahoosthitapragnya7890@gmail.com>
2025-08-02 23:08:07 +0530

    Fix case-sensitive imports for User model

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const User = require('../models/User');
 const generateToken = require('../utils/generateToken');
 
 exports.registerUser = async (req, res) => {
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const User = require('../models/User');
 const generateToken = require('../utils/generateToken');
 const router = express.Router();
 
